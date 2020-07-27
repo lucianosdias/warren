44c590d36c100079b11436b4cd95616d5a7c9b10
Luciano Dias <luciano.dias@softdesign.com.br>
2020-07-26 22:33:18 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,8 @@
 require('./bootstrap');
 require('./db').connect();
 const express = require('express');
-// const cron = require('node-cron');
 const accountsRoutes = require('./routes/accounts');
 const transactionsRoutes = require('./routes/transactions');
-
-// const incomeService = require('./services/incomeService');
-
-// const task = cron.schedule(
-//   `${process.env.JOB_CRON}`,
-//   incomeService.applyIncome,
-//   {
-//     scheduled: true,
-//     timezone: 'America/Sao_Paulo',
-//   }
-// );
-
-// task.start();
 
 const app = express();
 
